c2ad216fb163e2688c426adaedf0ea19fa50a89a
Ashevaxcode <ashevaxcode@gmail.com>
2022-09-24 06:43:27 +0000

    update app.py

--- app.py
+++ app.py
@@ -1,6 +1,45 @@
 print("asheva xcode")
 a = "asep"
-b= " Supriatna"
+b = " Supriatna"
 total = a+b
 print(total)
+
+# array
+c = [1,4,5,6]
+d = [3,"asep",67,"xcode",100]
+print(d[1] + d[3])
+
+
+# function
+def hitung(a,b):
+    print(a+b)
+    
+# memanggil function    
+hitung(3,4)
+
+# membuat volume 2 kubus
+def kubus1(a):
+    return a*a*a
+
+def kubus2(b):
+    return b*b*b
+
+def totalVKubus(a,b):
+    return a+b
+
+# pangggil total
+print(totalVKubus(kubus1(4),kubus2(8)))
+
+# while loop
+i = 1
+while i <= 6:
+    print (i)
+    i += 1 
+    
+# for
+fruit = ["banana","apple","grape","melon"]
+for x in fruit:
+    print(x)
+    
+
 
