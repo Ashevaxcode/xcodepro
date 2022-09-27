c289984296690b819d826a55120653e635da4fe4
Ashevaxcode <ashevaxcode@gmail.com>
2022-09-27 22:27:39 +0000

    add const funct

--- js-lanjutan/script.js
+++ js-lanjutan/script.js
@@ -17,10 +17,36 @@ function Mahasiswa(nama, energi) {
   let mahasiswa = {};
   mahasiswa.nama = nama;
   mahasiswa.energi = energi;
-
   mahasiswa.makan = function (porsi) {
-    mahasiswa.energi += porsi;
+    this.energi += porsi;
   };
 
-  return Mahasiswa;
+  mahasiswa.main = function (jam) {
+    this.energi -= jam;
+  };
+  //yang di return adalah let mahasiswa yang ada dalam function declaration
+  return mahasiswa;
 }
+
+const asheva = Mahasiswa("asep supriatna", 15);
+const doddy = Mahasiswa("doddy hermawan", 20);
+
+//3. constructor function
+
+function Mhs(nama, energi) {
+  // let mahasiswa = {};
+  this.nama = nama;
+  this.energi = energi;
+  this.makan = function (porsi) {
+    this.energi += porsi;
+  };
+
+  this.main = function (jam) {
+    this.energi -= jam;
+  };
+  //yang di return adalah let mahasiswa yang ada dalam function declaration
+  // return mahasiswa;
+}
+
+const saipul = new Mhs("saipul jamil", 40);
+const andre = new Mhs("andre taulani", 50);
