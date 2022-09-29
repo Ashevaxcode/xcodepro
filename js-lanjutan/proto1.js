61b9c7e2c2afe95e04c3bfba458e5843c2b059ba
Ashevaxcode <ashevaxcode@gmail.com>
2022-09-29 17:06:28 +0000

    UPDATE

--- js-lanjutan/proto1.js
+++ js-lanjutan/proto1.js
@@ -1,19 +1,20 @@
-User.prototype.damage = function(hit){
+User.prototype.damage = function (hit) {
   this.power -= hit;
-}
+};
 
-User.prototype.makan = function(porsi){
+User.prototype.makan = function (porsi) {
   this.power += porsi;
   return `Hai ${this.nama} Your power increase to ${this.power}`;
-}
-User.prototype.meta = function(jam){
+};
+User.prototype.meta = function (jam) {
   this.power += jam * 2;
   return `Hai ${this.nama} Your power increase to ${this.power}`;
-}
+};
 
-User.prototype.boost = function(jam){
+User.prototype.boost = function (jam) {
   this.power += jam * 4;
-  return `Hai ${this.nama} your power double boost ${jam * 4} become ${this.power}`;
-  
-  document.body.innerHTML = `Hai ${this.nama} your power double boost ${jam * 4} become ${this.power}`;
-}
+
+  return (document.body.innerHTML = `Hai ${this.nama} your power double boost ${
+    jam * 4
+  } become ${this.power}`);
+};
