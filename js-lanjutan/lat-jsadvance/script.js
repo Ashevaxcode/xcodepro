bd94f445c870f116000ca6e13ad3051f321b2ff7
ashevaxcode@gmail.com <ashevaxcode@gmail.com>
2022-10-10 19:54:24 +0700

    update js lanjut

--- js-lanjutan/lat-jsadvance/script.js
+++ js-lanjutan/lat-jsadvance/script.js
@@ -5,9 +5,28 @@ const jsLanjut = videos.filter((video)=>video.textContent.includes("JAVASCRIPT L
 
 //ambil durasi masing2 video
 //tanda titik adalah metode chaining
-.map(item=>item.dataset.duration);
+.map(item=>item.dataset.duration)
+//ini baru menampilkan durasi dengan tipe string
+
+//ubah durasi menjadi integer, dan menit menjadi detik, dan menghilangkan separator : menggunakan split
+.map(waktu=>{
+  const parts = waktu.split(":")
+  //ubah tiap durasi menjadi float
+  .map(part=>parseFloat(part))
+  
+  //jumlahkan menit dan detik
+  return (parts[0]*60+parts[1])
+})
+//jumlahkan seluruh durasi
+.reduce((acc,curr)=>acc+curr);
+
+//ubah ke dalam jam
+const jam=Math.floor(jsLanjut/3600);
+const menit=Math.floor((jsLanjut-(jam*3600))/60);
+const detik=Math.floor((jsLanjut-(jam*3600)-(menit*60)));
 
 
+const pDurasi = document.querySelector(".durasi");
+pDurasi.innerHTML=`${jam} jam ${menit} menit ${detik} detik`;
 
-
-console.log(jsLanjut);
+console.log(detik);
