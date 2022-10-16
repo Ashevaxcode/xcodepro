feef3e040c508c9fd39cf1da9ed5f125ccd209e5
Ashevaxcode <ashevaxcode@gmail.com>
2022-10-16 15:58:24 +0000

    spread

--- js-lanjutan/spread-operator/script.js
+++ js-lanjutan/spread-operator/script.js
@@ -7,4 +7,31 @@ const dosen = ["galih", "ade", "wanda"];
 // const orang = mhs.concat(dosen);
 //menggunakan spread operator ...(titik3)
 const orang = [...mhs, "ajis", ...dosen];
-console.log(...orang);
+console.log(orang.length);
+orang.forEach((m) => console.log(m));
+
+//spread operator dapat mengcopy array
+const asDosen = [...dosen];
+asDosen.push("asheva");
+asDosen.push("sandhika");
+console.log(asDosen);
+asDosen.shift();
+console.log(dosen);
+console.log(asDosen);
+
+//ambil element li
+const li = document.querySelectorAll("li");
+const mahasiswa = [];
+//for
+for (let i = 0; i < li.length; i++) {
+  mahasiswa.push(li[i].textContent);
+}
+//menggunakan map
+const siswa = [...li].map((m) => m.textContent);
+// console.log(siswa);
+const nama = document.querySelector(".nama");
+const huruf = [...nama.textContent].map((h) => `<span>${h}</span>`).join("");
+console.log(huruf);
+
+//kemudian kita timpa text h1 dengan huruf
+nama.innerHTML = huruf;
