5ee2f46577d5281d86eb63befcb1b7bf2dbc2374
ASHEVAXCODE <ashevaxcode@gmail.com>
2022-11-22 22:10:24 +0700

    latihan node

---
+++ node-latihan/cobaNode.js
@@ -0,0 +1,18 @@
+const nama = "ashevaXcode";
+const umur = 35;
+function sayHello(nama,umur){
+	console.log(`Hallo nama saya ${nama} , saya ${umur} tahun.`);
+	}
+	
+nama
+umur
+sayHello()
+sayHello(nama,umur)
+sayHello("sandhika",35)
+const sayHello2 =(nama,umur)=>{
+console.log(`hai saya ${nama} usia saya ${umur}`)
+}
+const sayHello2()
+sayHello2()
+sayHello2(umur, nama)
+sayHello2(nama, umur)
